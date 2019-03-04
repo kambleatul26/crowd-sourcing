47a0b643450760b1feb609c786a7b2d6017337a3
Atul Kamble <atulkamble@localhost.com>
2019-03-04 23:06:07 +0530

    design-break-fix

--- src/app/funds-list/funds-list.component.ts
+++ src/app/funds-list/funds-list.component.ts
@@ -22,24 +22,24 @@ export class FundsListComponent implements OnInit {
 
     ngOnInit() {
         this.CampaingList.push(new fundraiser_blog_post(
-            "My title",
-            "UserName",
+            "My title one",
+            "UserName one",
             10000,
-            `Struggling to pen down a fundraiser story on your own? No worries, just use the template below:Hi,My name is (your name) and I am raising funds for (name of your project). This initiative/project/film/novel is about (add a brief description). I am an aspiring director/ singer/ writer/ musician (write a bit about your background) and want to create this for (what is the purpose of the project? How will it benefit?). Please help me fulfill my dream by contributing towards my project or sharing this fundraiser with your friends and family! We are grateful for your help!`,5000
+            'A woman is the full circle. Within her is the power to create, nurture and transform.'
         ));
 
         this.CampaingList.push(new fundraiser_blog_post(
-            "My title",
-            "UserName",
-            10000,
-            `Struggling to pen down a fundraiser story on your own? No worries, just use the template below:Hi,My name is (your name) and I am raising funds for (name of your project). This initiative/project/film/novel is about (add a brief description). I am an aspiring director/ singer/ writer/ musician (write a bit about your background) and want to create this for (what is the purpose of the project? How will it benefit?). Please help me fulfill my dream by contributing towards my project or sharing this fundraiser with your friends and family! We are grateful for your help!`,5000
+            "My title two",
+            "UserName two",
+            20000,
+            'Women\'s empowerment is intertwined with respect for human rights.'
         ));
 
         this.CampaingList.push(new fundraiser_blog_post(
-            "My title",
-            "UserName",
-            10000,
-            `Struggling to pen down a fundraiser story on your own? No worries, just use the template below:Hi,My name is (your name) and I am raising funds for (name of your project). This initiative/project/film/novel is about (add a brief description). I am an aspiring director/ singer/ writer/ musician (write a bit about your background) and want to create this for (what is the purpose of the project? How will it benefit?). Please help me fulfill my dream by contributing towards my project or sharing this fundraiser with your friends and family! We are grateful for your help!`,5000
+            "My title three",
+            "UserName three",
+            30000,
+            `A woman is like a tea bag - you never know how strong she is until she gets in hot water.`
         ));
 
     }
